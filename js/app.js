e72726e04ff868b5877b9e9ba192ed01bc906fe3
showkhin1997 <showkhin15@gmail.com>
2023-06-01 11:34:37 +0600

    total-cost

--- js/app.js
+++ js/app.js
@@ -1,7 +1,3 @@
-// document.getElementById('player-select-button').addEventListener('click', function (e) {
-//     const playersName = document.getElementById('player-name');
-//     console.log(playersName);
-// })
 
 function playerButton(player, buttonId, isClick) {
     const name = document.getElementById(player);
@@ -12,11 +8,30 @@ function playerButton(player, buttonId, isClick) {
     }
 }
 
+
 function selectedPlayers(name) {
     const selectedPlayer = document.getElementById('selected-player');
     const selectedElement = document.createElement('li');
     selectedElement.classList.add('list-group-item', 'bg-black', 'text-white');
     selectedElement.innerText = name;
     selectedPlayer.appendChild(selectedElement);
-
 }
+
+
+document.getElementById('per-plyer-budget-calculate-button').addEventListener('click', function () {
+    const budgetInput = document.getElementById('budget-input');
+    const budgetAmmount = parseInt(budgetInput.value);
+    document.getElementById('player-expanse-cost').innerText = budgetAmmount * 5;
+})
+
+document.getElementById('total-cost-button').addEventListener('click', function () {
+    const magaerCostInput = document.getElementById('manager-cost');
+    const magaerCost = parseInt(magaerCostInput.value);
+    const coachCostInput = document.getElementById('coach-cost');
+    const coachCost = parseInt(coachCostInput.value);
+    const playerExpenseCost = document.getElementById('player-expanse-cost');
+    const playerExpenseCostValue = parseInt(playerExpenseCost.innerText);
+
+    const totalCost = playerExpenseCostValue + magaerCost + coachCost;
+    document.getElementById('total-cost').innerText = totalCost;
+})
